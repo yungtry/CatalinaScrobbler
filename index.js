999002078e6d1755391c86683190162afd1fe3f8
yungtry <yungtry@users.noreply.github.com>
2019-08-21 21:25:41 +0200

    autolaunch bug // packages update

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ var apple = require('./lib/apple.js')
 var win = require('./lib/window');
 
 app.on('ready', function() {
-  if (process.platform === "darwin"){
-    apple.detectPlayer().then(result => {
-      console.log("Player detected: "+result);
-      apple.replacePlayer(result);
-    }).catch(error => process.exit())
-    if (apple.rememberUser() === false){
-      app.emit('loginOpen');
-    }
-    else {
-      app.emit('trayCreate');
-    }
+    if (process.platform === "darwin"){
+        apple.autoLaunchEnabled().then(enabled => {
+            if (enabled === true) {
+                //Re-enable autolaunch because there was a bug when installing new version of the app.
+                apple.autoLaunchDisable();
+                apple.autoLaunchEnable();
+            }
+        });
+        apple.detectPlayer().then(result => {
+            console.log("Player detected: "+result);
+            apple.replacePlayer(result);
+        }).catch(error => process.exit())
+        if (apple.rememberUser() === false){
+            app.emit('loginOpen');
+        }
+        else {
+            app.emit('trayCreate');
+        }
   }
   else {
     console.log(process.platform + " is not supported.");
