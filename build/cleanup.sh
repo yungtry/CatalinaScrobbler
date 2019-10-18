ea57b87dd96b13ce5bbdc8674ad2da67e86dac84
yungtry <yungtry@users.noreply.github.com>
2019-10-18 22:21:42 +0200

    Create cleanup.sh

---
+++ build/cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+rm -rf CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/assets/photon/docs
+rm -rf CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/assets/photon/fonts
+rm -rf CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/assets/photon/sass
+rm -rf CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/assets/photon/dist/template-app
+rm CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/assets/photon/package.json
+rm CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/assets/photon/Gruntfile.js
+rm CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/assets/photon/CONTRIBUTING.md
+rm CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/assets/photon/CNAME
+rm CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/assets/photon/LICENSE
+rm CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/assets/photon/_config.yml
+rm CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/assets/photon/README.md
+
+rm CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/screenshot.png
+rm CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/LICENSE.md
+rm CatalinaScrobbler-darwin-x64/CatalinaScrobbler.app/Contents/Resources/app/README.md
